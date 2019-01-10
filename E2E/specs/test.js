6103a129b5b064bb4fe9b00e04985aa33f45d0af
Shahin <ghshahin@in.ibm.com>
2019-01-10 23:52:52 +0530

    Appium Protractor automation for web mobile app

--- E2E/specs/test.js
+++ E2E/specs/test.js
@@ -1,10 +1,10 @@
 //Run for hybrid application which build on browser and support mobile
 describe('Go to google', function (){
- it ('should go to google', function(){
+ it ('Search Daytona Systems', function(){
      browser.ignoreSynchronization =true;
-	 browser.get('http://www.google.lk');
-	 element(by.name('q')).sendKeys('selenium');
-	 element(by.name('q')).submit();
-	 element(by.linkText('Selenium IDE')).click();
+	 browser.get('https://www.google.com/');
+	 element(by.xpath("//*[@title='Search']")).sendKeys('Daytona Systems');
+	 element(by.name('btnK')).click();
+	 element(by.xpath("//span[contains(text(),'Daytona Systems (India) Pvt. Ltd.')]")).isDisplayed();
  });
  });
